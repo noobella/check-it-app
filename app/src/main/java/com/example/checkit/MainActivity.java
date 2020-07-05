998dbdd9148f700d6e9097a507cd64b15cdc55e7
Nabeeha Khurshid <nabeehak@icloud.com>
2020-07-05 15:22:17 +0100

    Add comments to MainActivity.java

--- app/src/main/java/com/example/checkit/MainActivity.java
+++ app/src/main/java/com/example/checkit/MainActivity.java
@@ -14,8 +14,19 @@ import com.google.android.material.tabs.TabLayoutMediator;
 
 import java.util.Calendar;
 
+/**
+ * App allows users to maintain checklists- a daily, weekly and monthly checklist
+ * User can add/edit/remove checklists, and set the status of the checklist using values from {@link ItemStatus}
+ * Each checklist will reset at the end of their designated time period e.g. daily resets everyday
+ * Old checklists can be accessible from archives
+ *
+ * @author Nabeeha Khurshid
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Class name for logging
+     */
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
@@ -23,14 +34,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /**
+         * Find views
+         */
         TabLayout tabLayout = findViewById(R.id.tablayout_activity_main);
         ViewPager2 viewPager2 = findViewById(R.id.viewpager_activity_main);
 
         final ListFragmentStateAdapter listFragmentStateAdapter = new ListFragmentStateAdapter(getSupportFragmentManager(), getLifecycle());
 
+        /**
+         * Set viewPager and set initial fragment to the second fragment (refer to {@link listFragmentStateAdapter#createFragment}
+         */
         viewPager2.setAdapter(listFragmentStateAdapter);
         viewPager2.setCurrentItem(1);
 
+        /**
+         * Set tabs to each fragment depending on the position value of {@link MainActivity#viewPager2}
+         */
         new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
             @Override
             public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
