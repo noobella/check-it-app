d745f446d17ec087680aaf542dfe39a469c5533c
Nabeeha Khurshid <nabeehak@icloud.com>
2020-09-06 12:43:19 +0100

    Add menu to activity_edit.xml

--- app/src/main/java/com/example/checkit/EditActivity.java
+++ app/src/main/java/com/example/checkit/EditActivity.java
@@ -1,7 +1,12 @@
 package com.example.checkit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 public class EditActivity extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class EditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        EditText editText = findViewById(R.id.activity_edit_edit_text);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_edit, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == R.id.menu_edit_save){
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
